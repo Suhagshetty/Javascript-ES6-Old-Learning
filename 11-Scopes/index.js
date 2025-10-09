e61e502dc57ed0dc2d7f2431dfba3577b00118de
Suhagshetty <suhagshetty07@gmail.com>
2025-10-09 12:39:20 +0530

    closures

--- 11-Scopes/index.js
+++ 11-Scopes/index.js
@@ -23,12 +23,42 @@ text();
 
 // Block Scope:- from es6 variables declared with let and const => block scope . var is => function scope
 
-{
-  let x = 10;
-  const y = 20;
-  var z = 30; // var is function-scoped, not block-scoped
-}
+// {
+//   let x = 10;
+//   const y = 20;
+//   var z = 30; // var is function-scoped, not block-scoped
+// }
 
-console.log(x); // ❌ undefined
-console.log(y); // ❌ undefined
-console.log(z); // ✅ works (var leaks out)
+// console.log(x); // ❌ undefined
+// console.log(y); // ❌ undefined
+// console.log(z); // ✅ works (var leaks out)
+
+// HINTS OF CLOSURES
+
+///////////////////// CLOSURES /////////////////////
+// A closure is created when a function remembers the variables from its other scope even after outer function has finished executing
+// A closure gives you access to an outer function’s variables from an inner function, even after the outer function has returned.
+
+function outer() {
+  let userName = "suhag";
+  function inner() {
+    console.log(`Hello ${userName}`);
+  }
+  return inner;
+}
+const greet = outer();
+greet();
+
+// A score tracker
+function scoreTracker() {
+  let score = 0;
+  return function () {
+    score++;
+    console.log(`The score is ${score}`);
+  };
+}
+const player1 = scoreTracker();
+const player2 = scoreTracker();
+player1();
+player1();
+player2();
